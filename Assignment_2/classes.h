03b62d211b65b30b9ede81f444b24e0dab39b69e
Kaelan Trowbridge <trowbrka@oregonstate.edu>
2024-01-27 22:27:02 -0800

    Files are now being read and records being stored. Records not yet added to storagebuffermanager

--- Assignment_2/classes.h
+++ Assignment_2/classes.h
@@ -31,17 +31,17 @@ class StorageBufferManager {
 
 private:
     
-    const int BLOCK_SIZE = ; // initialize the  block size allowed in main memory according to the question 
-
+    const int BLOCK_SIZE = 3; // initialize the  block size allowed in main memory according to the question 
+    int numRecords;
     // You may declare variables based on your need 
-
+    //const int BLOCK_SIZE = 4096;
     // Insert new record 
     void insertRecord(Record record) {
 
         // No records written yet
         if (numRecords == 0) {
             // Initialize first block
-
+            
         }
         // Add record to the block
 
@@ -63,7 +63,38 @@ public:
 
     // Read csv file (Employee.csv) and add records to the (EmployeeRelation)
     void createFromFile(string csvFName) {
-        
+        ifstream file(csvFName);
+        string line;
+        vector<Record> records; // TEMPORARY keep records in a vector for testing
+
+        // check if the file opened
+        if (!file.is_open()) {
+            cerr << "Cannot open file\n";
+            return;
+        }
+
+        // read each line from the file
+        while (getline(file, line)) {
+            stringstream ss(line);
+            string field;
+            vector<string> fields; // record fields
+
+            // split the line into fields using comma delim
+            while (getline(ss, field, ',')) {
+                fields.push_back(field);
+            }
+
+            // create record object from the fields and store in vector
+            records.push_back(Record(fields)); // TEMP use insertrecords once implemented
+        }
+
+        file.close();
+
+        // TEMP print the records to see if working
+        for (Record& record : records) {
+            record.print();
+        }
+
     }
 
     // Given an ID, find the relevant record and print it
